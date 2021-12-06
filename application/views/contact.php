7496ff6d441db6f00051375ba19128e7de43e95a
Kenedy Cheruiyot <kenedy@localhost.localdomain>
2021-12-06 05:57:21 +0300

    contact done

--- application/views/contact.php
+++ application/views/contact.php
@@ -14,11 +14,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <!-- ======= Hero Section ======= -->
 
     <main id="main">
-    <section id="breadcrumbs" class="breadcrumbs">
+        <section id="breadcrumbs" class="breadcrumbs">
             <div class="container">
 
                 <div class="d-flex justify-content-between align-items-center">
-                   
+
                     <ol>
                         <li>Contact Us</li>
                     </ol>
@@ -63,7 +63,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 </div>
                 <div class="row mt-5 justify-content-center" data-aos="fade-up">
                     <div class="col-lg-10">
-                        <form  method="post" role="form" class="php-email-form">
+                        <form id="contactform" class="php-email-form">
                             <div class="row">
                                 <div class="col-md-6 form-group">
                                     <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
@@ -76,9 +76,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                 <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
                             </div>
                             <div class="form-group mt-3">
-                                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-                            </div>                            
-                            <div class="text-center "><button id="submitbtn" class="btn-success p-2 mt-2" style="border-radius: 24px;">Send Message</button></div>
+                                <textarea class="form-control" id="message" name="message" rows="5" placeholder="Message" required></textarea>
+                            </div>
+                            <div class="text-center "><input type="button" id="submitbtn" class="btn-success p-2 mt-2" style="border-radius: 24px;" value="Send Message" /></div>
                         </form>
                     </div>
 
@@ -86,7 +86,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
             </div>
         </section><!-- End Contact Section -->
-        
+
         <div class="container-fliud p-2" data-aos="fade-up">
 
             <div class="row content">
@@ -94,28 +94,43 @@ defined('BASEPATH') or exit('No direct script access allowed');
                     <h3>
                         Help us continue to empower youths in Kenya and worldwide.
                     </h3>
-                </div>                
+                </div>
             </div>
 
         </div>
     </main><!-- End #main -->
 
     <!-- ======= Footer ======= -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <?php include 'footer.php' ?>
     <script>
-          $(document).ready(function() {
-            $("#submitbtn").click(function (event) {
+        $(document).ready(function() {
+            // $("#submitbtn").click(function (event) {
+            $('#submitbtn').on('click', function(event) {
                 event.preventDefault();
                 event.stopImmediatePropagation();
-                var formValues = $(this).serialize();
+                var formData = {
+                    name: $("#name").val(),
+                    email: $("#email").val(),
+                    subject: $("#subject").val(),
+                    message: $("#message").val(),                    
+                };
 
-                $.post("/Home/contactpost", formValues, function(data) {
-                    // Display the returned data in browser
-                    
+                // var formValues = $("#contactform").serialize();
+                $.ajax({
+                    type: "POST",
+                    url: "/Home/contactpost",
+                    data: formData,
+                    dataType: "json",
+                    encode: true,
+                }).done(function(data) {
+                    console.log(data);
                 });
+
+
             });
         });
-        </script>
+    </script>
 </body>
 
 </html>
